6a41d1e6320b45d5ef09d1a694f57a2daea73fe0
alexcurwood <149108865+alexcurwood@users.noreply.github.com>
2024-04-15 21:27:10 +0100

    created playlist cards

--- src/app/playlists/playlists.jsx
+++ src/app/playlists/playlists.jsx
@@ -1,11 +1,14 @@
 import './playlists.css'
 
 export default function Playlists({playlistIds, playlistNames}){
+    function handleClick(){
+        
+    }
     let tags = playlistNames.map(playlist =>
         <div className='playlistContainer'>
             <p>{playlist}</p>
         {/* <iframe className = 'playlist' style={{borderRadius:12+"px"}} src={`https://open.spotify.com/embed/playlist/${playlist}?utm_source=generator`}  height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe> */}
-        <button className='playlistButton'>Use Playlist</button>
+        <button className='playlistButton' onClick={handleClick}>Use Playlist</button>
         </div>)
    return (
     <div className="playlists">
