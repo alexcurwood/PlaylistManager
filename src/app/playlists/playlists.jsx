e26be45d43cd0323980cfc79b80358b730916e77
alexcurwood <149108865+alexcurwood@users.noreply.github.com>
2024-04-16 22:46:15 +0100

    Started createGenreButtons

--- src/app/playlists/playlists.jsx
+++ src/app/playlists/playlists.jsx
@@ -3,11 +3,39 @@ import { useState } from "react";
 
 export default function Playlists({ playlists }) {
   const [displayPlaylists, setDisplayPlaylists] = useState(true);
+  const [genreButtons, setGenreButtons] = useState();
   const [playlistId, setPlaylistId] = useState();
 
   function handleClick(e) {
     setDisplayPlaylists(false);
     setPlaylistId(e.target.className);
+    createGenreButtons(e.target.className);
+  }
+
+  async function createGenreButtons(playlistId) {
+    const access_token = localStorage.getItem("access_token");
+    const response = await fetch(
+      `https://api.spotify.com/v1/playlists/${playlistId}`,
+      {
+        headers: {
+          Authorization: "Bearer " + access_token,
+        },
+      }
+    );
+    const json = await response.json();
+    let genresArray = [];
+    json.tracks.items.map((track) => {
+      console.log(track.track.artists[0].genres);
+      for (genre of track.track.artists[0].genres) {
+        if (!genre in genresArray) {
+          genresArray.append(genre);
+        }
+      }
+    });
+    let genreButtons = genresArray.map((genre) => {
+      <button>{genre}</button>;
+    });
+    setGenreButtons(genreButtons);
   }
 
   function handleBack() {
@@ -31,6 +59,7 @@ export default function Playlists({ playlists }) {
         !displayPlaylists && (
           <div>
             <button onClick={handleBack}>Back</button>
+            <div className="genre-buttons">{genreButtons}</div>
             <iframe
               className="playlist"
               style={{ borderRadius: 12 + "px" }}
